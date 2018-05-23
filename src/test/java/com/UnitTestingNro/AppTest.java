f789eb8e0350bed68b80791823a5d10295fc3959
Flor21 <fas-2196@hotmail.com>
2018-05-23 12:16:50 -0300

    Unit Testing

---
+++ src/test/java/com/UnitTestingNro/AppTest.java
@@ -0,0 +1,18 @@
+package com.UnitTestingNro;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+  private static App obj_prueba;
+  
+  @Test
+    public void compararTest () {
+	 
+	 assertTrue ("El primer numero es menor que el segundo", obj_prueba.comparar( 78 ,9)); 
+	 }
+  
+}
